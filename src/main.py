d1093a3e96455b1e1d402eaddd3f3f3a515f42f5
Sere Otubu <sereotubu@gmail.com>
2026-02-04 18:21:25 -0500

    made configurations to run on server applications

--- src/main.py
+++ src/main.py
@@ -61,6 +61,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "https://dev-group-16-capstone.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
