ebe7713bc02a744a5803fd560d2d5c894bcfffa2
Sere Otubu <sereotubu@gmail.com>
2026-02-27 21:52:19 -0500

    Test bug issue fix
    When tests import main.py to get the FastAPI app, it imports both routers from routes.py triggering the DB connection.
    - added a way to make the RAG repository singleton lazy-loaded so it's only created when actually needed (not at import time)

--- src/rag/__init__.py
+++ src/rag/__init__.py
@@ -2,8 +2,8 @@
 Python package for Retrieval-Augmented Generation (RAG) functionalities.
 """
 
-from .repositories import RAGRepository, rag_repository # data access layer for RAG
+from .repositories import RAGRepository, get_rag_repository_instance # data access layer for RAG
 from .services import RAGService # business logic layer for RAG
 
 # Only expose the RAGService and RAGRepository at the package level
-__all__ = ["RAGService", "RAGRepository", "rag_repository"]
+__all__ = ["RAGService", "RAGRepository", "get_rag_repository_instance"]
